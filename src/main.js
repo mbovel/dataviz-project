856987b7bbb4fbd115723e2aebef1001de88ca1f
Matthieu Bovel <matthieu@bovel.net>
2018-12-11 23:01:31 +0100

    Start final visualization

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 const graphPlot = new GraphPlot(document.getElementById("plot"));
 const sidebar = new Sidebar(document.getElementById("sidebar"), graphPlot);
+
+async function init() {
+	const data = await getData(2018, 12, 11);
+	graphPlot.updateData(data);
+}
+
+init().catch(console.error);
