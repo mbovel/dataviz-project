2ddd1e0d51a731ded3308297f3885a99974e62ee
Matthieu Bovel <matthieu@bovel.net>
2018-12-11 23:31:33 +0100

    Add regions to main sources dataset

--- scripts/generate_sources.py
+++ scripts/generate_sources.py
@@ -7,7 +7,7 @@ import requests
 from utils import SOURCES_FILE
 
 url = "https://en.wikipedia.org/w/api.php"
-title = "Wikipedia:News sources/Africa"
+
 regions = "Africa", "Americas", "Oceania", "Europe", "China", "India", "Asia"
 
 
@@ -25,9 +25,9 @@ def get_wiki_links(pagetitle):
 
 with open(SOURCES_FILE, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(['domain', 'name'])
+    writer.writerow(['domain', 'name', 'region'])
     for region in regions:
         for link, name in get_wiki_links("Wikipedia:News sources/" + region):
             uri_parsed = urlparse(link)
             domain = uri_parsed.netloc.replace('www.', '')
-            writer.writerow([domain, name])
+            writer.writerow([domain, name, region])
